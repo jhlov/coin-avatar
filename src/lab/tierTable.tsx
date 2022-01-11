d41705ee732f6589e7826b89d4491485c710d13e
jhlov <jhlovv@gmail.com>
2022-01-11 21:55:19 +0900

    전략 세팅

--- src/lab/tierTable.tsx
+++ src/lab/tierTable.tsx
@@ -67,6 +67,578 @@ const getTierTable = (strategy: string): TierTableData[] => {
         seed: (1 / 12) * 100
       }
     ];
+  } else if (strategy === "new_poten_13tier") {
+    return [
+      {
+        buy: 0,
+        sell: {
+          type: "yield",
+          value: 6
+        },
+        seed: (1 / 6) * 100
+      },
+      {
+        buy: -1.5,
+        sell: {
+          type: "yield",
+          value: 4
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -1.5,
+        sell: {
+          type: "yield",
+          value: 4
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -1.5,
+        sell: {
+          type: "yield",
+          value: 4
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -1,
+        sell: {
+          type: "yield",
+          value: 5
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -2,
+        sell: {
+          type: "yield",
+          value: 7
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -2.5,
+        sell: {
+          type: "yield",
+          value: 7
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -2.5,
+        sell: {
+          type: "yield",
+          value: 8
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -2.5,
+        sell: {
+          type: "yield",
+          value: 9
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -7,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 24) * 100
+      },
+      {
+        buy: -7,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 24) * 100
+      },
+      {
+        buy: -15,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 24) * 100
+      },
+      {
+        buy: -20,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 24) * 100
+      }
+    ];
+  } else if (strategy === "new_poten_17tier") {
+    return [
+      {
+        buy: 0,
+        sell: {
+          type: "yield",
+          value: 6
+        },
+        seed: (1 / 6) * 100
+      },
+      {
+        buy: -1.5,
+        sell: {
+          type: "yield",
+          value: 4
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -1.5,
+        sell: {
+          type: "yield",
+          value: 4
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -1.5,
+        sell: {
+          type: "yield",
+          value: 4
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -1,
+        sell: {
+          type: "yield",
+          value: 5
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -2,
+        sell: {
+          type: "yield",
+          value: 7
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -2.5,
+        sell: {
+          type: "yield",
+          value: 7
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -2.5,
+        sell: {
+          type: "yield",
+          value: 8
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -2.5,
+        sell: {
+          type: "yield",
+          value: 9
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -4,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 24) * 100
+      },
+      {
+        buy: -4,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 24) * 100
+      },
+      {
+        buy: -4,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 48) * 100
+      },
+      {
+        buy: -4,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 48) * 100
+      },
+      {
+        buy: -8,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 96) * 100
+      },
+      {
+        buy: -8,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 96) * 100
+      },
+      {
+        buy: -12,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 96) * 100
+      },
+      {
+        buy: -12,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 96) * 100
+      }
+    ];
+  } else if (strategy === "new_poten_20tier") {
+    return [
+      {
+        buy: 0,
+        sell: {
+          type: "yield",
+          value: 6
+        },
+        seed: (1 / 6) * 100
+      },
+      {
+        buy: -1.5,
+        sell: {
+          type: "yield",
+          value: 4
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -1.5,
+        sell: {
+          type: "yield",
+          value: 4
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -1.5,
+        sell: {
+          type: "yield",
+          value: 4
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -1,
+        sell: {
+          type: "yield",
+          value: 5
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -2,
+        sell: {
+          type: "yield",
+          value: 7
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -2.5,
+        sell: {
+          type: "yield",
+          value: 7
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -2.5,
+        sell: {
+          type: "yield",
+          value: 8
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -2.5,
+        sell: {
+          type: "yield",
+          value: 9
+        },
+        seed: (1 / 12) * 100
+      },
+      {
+        buy: -4,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 48) * 100
+      },
+      {
+        buy: -4,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 48) * 100
+      },
+      {
+        buy: -4,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 48) * 100
+      },
+      {
+        buy: -4,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 48) * 100
+      },
+      {
+        buy: -10,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 48) * 100
+      },
+      {
+        buy: -10,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 96) * 100
+      },
+      {
+        buy: -12,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 96) * 100
+      },
+      {
+        buy: -12,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 96) * 100
+      },
+      {
+        buy: -12,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 96) * 100
+      },
+      {
+        buy: -12,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 96) * 100
+      },
+      {
+        buy: -12,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (1 / 96) * 100
+      }
+    ];
+  } else if (strategy === "del_poten_20tier") {
+    return [
+      {
+        buy: 0,
+        sell: {
+          type: "yield",
+          value: 6
+        },
+        seed: (1 / 6) * 100
+      },
+      {
+        buy: -1.5,
+        sell: {
+          type: "yield",
+          value: 4
+        },
+        seed: (7 / 114) * 100
+      },
+      {
+        buy: -1.5,
+        sell: {
+          type: "yield",
+          value: 4
+        },
+        seed: (7 / 114) * 100
+      },
+      {
+        buy: -1.5,
+        sell: {
+          type: "yield",
+          value: 4
+        },
+        seed: (7 / 114) * 100
+      },
+      {
+        buy: -1,
+        sell: {
+          type: "yield",
+          value: 5
+        },
+        seed: (7 / 114) * 100
+      },
+      {
+        buy: -2,
+        sell: {
+          type: "yield",
+          value: 7
+        },
+        seed: (7 / 114) * 100
+      },
+      {
+        buy: -2.5,
+        sell: {
+          type: "yield",
+          value: 7
+        },
+        seed: (7 / 114) * 100
+      },
+      {
+        buy: -2.5,
+        sell: {
+          type: "yield",
+          value: 8
+        },
+        seed: (7 / 114) * 100
+      },
+      {
+        buy: -2.5,
+        sell: {
+          type: "yield",
+          value: 9
+        },
+        seed: (7 / 114) * 100
+      },
+      {
+        buy: -4,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (5 / 114) * 100
+      },
+      {
+        buy: -4,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (4 / 114) * 100
+      },
+      {
+        buy: -4,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (4 / 114) * 100
+      },
+      {
+        buy: -4,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (4 / 114) * 100
+      },
+      {
+        buy: -10,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (4 / 114) * 100
+      },
+      {
+        buy: -10,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (3 / 114) * 100
+      },
+      {
+        buy: -12,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (3 / 114) * 100
+      },
+      {
+        buy: -12,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (3 / 114) * 100
+      },
+      {
+        buy: -12,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (3 / 114) * 100
+      },
+      {
+        buy: -12,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (3 / 114) * 100
+      },
+      {
+        buy: -12,
+        sell: {
+          type: "tier",
+          value: 2
+        },
+        seed: (3 / 114) * 100
+      }
+    ];
   }
 
   return [];
